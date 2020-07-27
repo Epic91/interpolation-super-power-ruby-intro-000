79313a4609311427311d05794e70140ebd53794f
Evelyn Hernandez <hernandeze791@gmail.com>
2020-07-27 15:14:16 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,7 +1,7 @@
 
 describe "display_rainbow" do
   it "accepts one argument" do
-    colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+    colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
 
     allow(self).to receive(:puts)
 
