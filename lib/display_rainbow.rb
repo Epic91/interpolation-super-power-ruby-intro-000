8d8c73680953a68e66892a832d592f154799482d
Evelyn Hernandez <hernandeze791@gmail.com>
2020-07-27 15:29:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
 def display_rainbow(colors = "red", "orange", "yellow", "green", "blue", "indigo", "violet")
-  puts "red: #{[0]}"
-  puts "orange: #{[1]}"
-  puts "yellow: #{[2]}"
-  puts "green: #{[3]}"
-  puts "blue: #{[4]}"
-  puts "indigo: #{[5]}"
-  puts "violet: #{[6]}"
+  puts "red: #{(0)}"
+  puts "orange: #{(1)}"
+  puts "yellow: #{(2)}"
+  puts "green: #{(3)}"
+  puts "blue: #{(4)}"
+  puts "indigo: #{(5)}"
+  puts "violet: #{(6)}"
 end
